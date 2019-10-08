2d8254ea43fb5c90a2af74232567632f7380ae8c
Alison Vermeil <alison.minton@gmail.com>
2019-10-08 15:17:52 -0600

    "Refactor ship test to use def setup"
    Co-Authored-By: William Meighan <williamdmeighan@gmail.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,41 +6,37 @@ require "pry"
 
 class TestShip < Minitest::Test
 
-  def test_it_exists
-    cruiser = Ship.new("Cruiser", 3)
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
-    assert_instance_of Ship, cruiser
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
   end
 
   def test_it_has_a_name
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal "Cruiser", cruiser.name
+    assert_equal "Cruiser", @cruiser.name
   end
 
   def test_it_has_a_length
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal 3, cruiser.length
+    assert_equal 3, @cruiser.length
   end
 
   def test_is_has_health
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal 3, cruiser.health
+    assert_equal 3, @cruiser.health
   end
 
   def test_health_equals_length
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal true, cruiser.length == cruiser.health
+    assert_equal true, @cruiser.length == @cruiser.health
   end
 
-  def test_ship_begins_unsunk
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal false, cruiser.sunk?
+  def test_ship_begins_not_sunk
+    assert_equal false, @cruiser.sunk?
   end
-  
+
+  def test_it_can_get_hit
+
+  end
+
 
 end
