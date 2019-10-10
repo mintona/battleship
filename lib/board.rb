da4bc148f66f2ed146403cab38b93cabc5535631
Alison Vermeil <alison.minton@gmail.com>
2019-10-10 12:26:09 -0600

    "Add methods for validating cell coordinates and ship placements to board class plus preliminary associated tests"
    Co-Authored-By: William Meighan <williamdmeighan@gmail.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,63 @@
 class Board
-  attr_accessor :cells
 
   def initialize
-    #@cells = Hash.new(0)
   end
 
   def cells
-    coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-
-    cells = Hash.new { |hash, key| hash[key] = [] }
-    coordinates.each do |coordinate|
-      cells[coordinate] << Cell.new(coordinate)
-    end
-    cells
+    {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4")
+    }
   end
+
+  def valid_coordinate?(coordinate)
+    cells.keys.include?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    coords = cells.keys
+    consecutive_coordinates = []
+    coords.each_cons(coordinates.length) { |coord| consecutive_coordinates << coord }
+
+    if (ship.length == coordinates.length) && consecutive_coordinates.include?(coordinates)
+      true
+    else
+      false
+    end
+      # If coordinates are consecutive
+
+
+
+      # If coordinates aren't diagnol
+#
+    # Test must be consecutive coordinates
+    # Test can't be diagnol coordinates
+    # Test it still passes given all of that criteria
+  end
+
+
+  # def cells - maybe we can make this work another way later... it's returning the new cell in an array and we don't want it in an array
+  #   coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+  #   cells = Hash.new { |hash, key| hash[key] = [] }
+  #   @coordinates.each do |coordinate|
+  #     cells[coordinate] << Cell.new(coordinate)
+  #   end
+  #   cells
+  # end
 end
 
 
--- test/board_test.rb
+++ test/board_test.rb
@@ -9,6 +9,8 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -19,10 +21,57 @@ class BoardTest < Minitest::Test
     assert_instance_of Hash, @board.cells
 
     assert_equal 16, @board.cells.keys.length
-#binding.pry
+    assert_equal 16, @board.cells.values.length
 
-    assert_instance_of Cell, @board.cells.values[0][0]
+    cell_value_class = @board.cells.values.all? { |value| value.class == Cell }
+
+    assert_equal true, cell_value_class
 
   end
+
+   def test_board_cells_can_be_validated
+
+     assert_equal true, @board.valid_coordinate?("A1")
+     assert_equal true, @board.valid_coordinate?("D4")
+
+     assert_equal false, @board.valid_coordinate?("A5")
+     assert_equal false, @board.valid_coordinate?("E1")
+   end
+
+   def test_it_can_validate_placement_of_ships_based_on_ship_length
+#binding.pry
+
+     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+     assert_equal true, @board.valid_placement?(@submarine, ["B3", "B4"])
+
+     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+
+   end
+
+   def test_it_can_validate_placement_of_ships_based_on_consecutive_coordinates
+     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+
+     assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+
+     assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+
+     assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"]) # This has something to do with B1 C1 being consecutive.
+   end
+#
+#   def test_it_can_validate_placement_of_ships_on_the_diagnol
+# skip
+#     assert_equal false, @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+#
+#     assert_equal false, @board.valid_placement?(submarine, ["C2", "D3"])
+#
+#   end
+#
+#   def test_it_can_tell_valid_placement_based_on_criteria_laid_out_in_previous_tests
+#     skip
+#     assert_equal true, @board.valid_placement?(submarine, ["A1", "A2"])
+#
+#     assert_equal true, @valid_placement?(cruiser, ["B1", "C1", "D1"])
+#   end
 
 end
