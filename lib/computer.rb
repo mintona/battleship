a815f3a7c3449a542d7a51b620da9055ac455413
Alison Vermeil <alison.minton@gmail.com>
2019-10-12 18:59:51 -0600

    Refactor computer class to remove unnecessary methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,9 +1,13 @@
 class Computer
-  attr_reader :cruiser, :submarine
+  attr_reader :ships
 
   def initialize
-    @cruiser = Ship.new(cruiser, 3)
-    @submarine = Ship.new(submarine, 2)
+    @ships = []
+  end
+
+  def add_ships
+    @ships << cruiser = Ship.new("Cruiser", 3)
+    @ships << submarine = Ship.new("Submarine", 2)
   end
 
   # def access_ships
@@ -15,7 +19,7 @@ class Computer
     # conform to all the stuff about valid coordinates.
   end
 
-  def place_ships
+  def place_ships(gameboard)
     # cruiser = Ship.new("Cruiser", 3)
     # submarine = Ship.new("Submarine", 2)
 
@@ -23,11 +27,11 @@ class Computer
     # board.add_cells
 
     # These arrays for coordinate placement need to be randomized.
-    play.board.place(cruiser, ["A1", "A2", "A3"])
-    play.board.place(submarine, ["D3", "D4"])
-
-    #only here so we can see what the computer is doing with the hard coded coordinates above
-    puts play.board.render(true)
+    # gameboard.place(cruiser, ["A1", "A2", "A3"])
+    # gameboard.place(submarine, ["D3", "D4"])
+    #
+    # #only here so we can see what the computer is doing with the hard coded coordinates above
+    # puts play.board.render(true)
   end
 
 end
