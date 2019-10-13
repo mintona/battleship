b40c71a6b0ea44d2925293aad458f48512badb87
William Meighan <williamdmeighan@gmail.com>
2019-10-13 15:34:54 -0600

    Add computer_places_ships method

--- lib/round.rb
+++ lib/round.rb
@@ -7,13 +7,15 @@ class Round
     @computer_player = nil
     @human_gameboard = {}
     @human_player = nil
+
   end
 
   def play_game
       create_a_board_with_cells
       create_computer_player
       create_human_player
-      computer_takes_first_turn
+    #  computer_takes_first_turn
+      computer_places_ships
       human_takes_first_turn
   end
 
@@ -35,14 +37,35 @@ class Round
     @human_player.add_ships
   end
 
-  def computer_takes_first_turn
-    # this is actually where we will do the random generator, not in the ship class.
-    @computer_gameboard.place(@computer_player.ships[0], ["A1", "A2", "A3"])
-    @computer_gameboard.place(@computer_player.ships[1], ["D3", "D4"])
-    @computer_gameboard
+  def computer_places_ships
+    @computer_player.ships.each do |ship|
+      random_coordinates = @computer_gameboard.cells.keys.sample(ship.length)
+      until @computer_gameboard.valid_placement?(ship, random_coordinates) == true
+        random_coordinates = @computer_gameboard.cells.keys.sample(ship.length)
+      end
+      @computer_gameboard.place(ship, random_coordinates)
+    end
+
+
+      # until @computer_gameboard.valid_placement?(@computer_player.ships[0], cruiser_coordinates)
+      #   cruiser_coordinates = coordinates_array.sample(3)
+      # end
+
+#      until @computer_gameboard.valid_placement?(@computer_player.ships
+
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
+
   end
+
+  # def computer_takes_first_turn
+  #   # this is actually where we will do the random generator, not in the ship class.
+  #   @computer_gameboard.place(@computer_player.ships[0], ["A1", "A2", "A3"])
+  #   @computer_gameboard.place(@computer_player.ships[1], ["D3", "D4"])
+  #   @computer_gameboard
+  #   puts "I have laid out my ships on the grid."
+  #   puts "You now need to lay out your two ships."
+  # end
 
   def human_takes_first_turn
     puts "The Cruiser is two units long and the Submarine is three units long."
