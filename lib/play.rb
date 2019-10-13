92f2429816844c30dfd2d1fe613211d1c02d324f
Alison Vermeil <alison.minton@gmail.com>
2019-10-12 19:00:37 -0600

    Add to play class to add additional steps to game play and update runner file to check progress

--- lib/play.rb
+++ lib/play.rb
@@ -1,42 +1,74 @@
 class Play
-  attr_accessor :play
+  attr_accessor :start, :gameboard, :computer_player, :human_player
 
   def initialize
-    @play = false
-    @board = {}
+    @start = false
+    @gameboard = {}
+    @computer_player = nil
+    @human_player = nil
   end
 
-  def greeting
-    puts "Welcome to BATTLESHIP\n"
-  end
+  # def greeting
+  #   puts "Welcome to BATTLESHIP\n\n"
+  # end
 
-  def play?
-    puts "Enter P to play. Enter Q to quit"
-    print ">"
-    answer = gets.chomp
-    if answer.downcase == "p"
-      @play = true
-    elsif answer.downcase == "q"
-      @play = false
+  def play_game
+    if @start == true
+      create_a_board_with_cells
+      create_computer_player
+      computer_takes_first_turn
+      puts "I have laid out my ships on the grid."
+      puts "You now need to lay out your two ships"
+      puts "The Cruiser is two units long and the Submarine is three units long."
+      puts "  1 2 3 4"
+      puts "A . . . ."
+      puts "B . . . ."
+      puts "C . . . ."
+      puts "D . . . ."
+      puts "Enter the squares for the Cruiser (3 spaces), ex: B1 B2 B3"
+      print ">"
+      user_coordinates = gets.chomp.upcase.split
+      @gameboard.validate_placement?(@human_player.ships[0], user_coordinates) # need to created human player class, add the ships, etc.
     else
-      puts "Please enter a valid input."
-      # Add code to re-run the code until the user either says yes or no
+      return nil
     end
+
+    # puts "Enter P to play. Enter Q to quit"
+    # print ">"
+    # answer = gets.chomp
+    # if answer.downcase == "p"
+    #   @start = true
+    #   create_a_board_with_cells
+    #   create_computer_player
+    # elsif answer.downcase == "q"
+    #   @start = false
+    # else
+    #   puts "Please enter a valid input."
+    #   # Add code to re-run the code until the user either says yes or no
+    # end
   end
 
   def create_a_board_with_cells
-    if @play == true
-     @board = Board.new
-     @board.add_cells
-   end
-   #binding.pry
-   @board.cells
-
+    @gameboard = Board.new
+    @gameboard.add_cells
+    @gameboard
   end
 
+  def create_computer_player
+    @computer_player = Computer.new
+    @computer_player.add_ships
+  end
+
+  # def create_human_player
+  #   @human_player = Human.new
+      #@human_player.add_ships
+  # end
+
   def computer_takes_first_turn
-    computer = Computer.new
-    computer.place_ships
+    # this is actually where we will do the random generator, not in the ship class.
+    @gameboard.place(@computer_player.ships[0], ["A1", "A2", "A3"])
+    @gameboard.place(@computer_player.ships[1], ["D3", "D4"])
+    @gameboard
   end
 
 
--- runner.rb
+++ runner.rb
@@ -4,18 +4,20 @@ require_relative './lib/board.rb'
 require_relative './lib/play.rb'
 require_relative './lib/computer.rb'
 require_relative './lib/person.rb'
+require_relative './lib/start.rb'
 require 'pry'
 
 
-play = Play.new
-#computer = Computer.new
-#person = Person.new
-
-
-play.greeting
-play.play?
-play.create_a_board_with_cells
-binding.pry
-play.computer_takes_first_turn
+# play = Play.new
+#
+# play.greeting
+# play.play?
+# play.computer_takes_first_turn
+# puts play.gameboard.render(true)
 
 # might need to create a board here and then have the methods of play take the argument of the board so it can access the cells of the board
+
+start = Start.new
+
+start.greeting
+start.play?
