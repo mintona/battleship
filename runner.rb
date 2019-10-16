dbe889bb299cef4eebcfbaddd430f03eb44013eb
William Meighan <williamdmeighan@gmail.com>
2019-10-16 15:12:48 -0600

    Update runner with final code
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- runner.rb
+++ runner.rb
@@ -1,23 +1,11 @@
 require_relative './lib/ship.rb'
 require_relative './lib/cell.rb'
 require_relative './lib/board.rb'
-require_relative './lib/round.rb'
 require_relative './lib/computer.rb'
 require_relative './lib/player.rb'
-require_relative './lib/start.rb'
 require_relative './lib/gameplay.rb'
 require_relative './lib/prep.rb'
 require_relative './lib/turn.rb'
-require 'pry'
-
-# The code below works for master as it is as of 10/15 @ 4 pm
-# start = Start.new
-# start.greeting
-# until start.play_game == false
-#   start.play?
-# end
-
-
 
 game = Gameplay.new
 
