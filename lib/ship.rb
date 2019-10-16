cd700a2dd49051ab73ea86a94f96f41ef30a5f43
William Meighan <williamdmeighan@gmail.com>
2019-10-16 15:39:06 -0600

    Refactor ship class and corresponding tests
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,7 @@
 class Ship
 
-  attr_reader :name, :length, :sunk
-  attr_accessor :health
+  attr_reader :name, :length
+  attr_accessor :health, :sunk
 
   def initialize(name, length)
     @name = name
@@ -11,14 +11,14 @@ class Ship
   end
 
   def sunk?
-    if @health == 0
-      @sunk = true
-    end
     @sunk
   end
 
   def hit
     @health -= 1
+    if @health == 0
+      @sunk = true
+    end
   end
 
 end
